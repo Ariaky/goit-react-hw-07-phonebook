033271a838ba2cbceccead3d3e08674320ac4e5c
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-04-29 23:00:36 +0200

    commit

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -2,3 +2,5 @@ import axios from "axios";
 import { toast } from 'react-toastify';
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
+axios.defaults.baseURL = 'https://6630049dc92f351c03d8d7aa.mockapi.io';
+
