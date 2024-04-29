efd9d08e5c927f29b2da3201ce094a761819e86e
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-04-29 23:04:27 +0200

    commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 
+
 export const store = configureStore({
     reducer: {
-      contacts: '',
-      filter: '',
+      contacts: contactsReducer,
+      filter: filterReducer,
     },
   });
