42d906b50ee14f8ea32651716ec11d64b35a950c
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-04-30 17:17:04 +0200

    operations

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -4,3 +4,50 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 axios.defaults.baseURL = 'https://6630049dc92f351c03d8d7aa.mockapi.io';
 
+export const fetchContacts = createAsyncThunk(
+    'contacts/fetchAll',
+    async (_, thunkAPI) => {
+      try {
+        const response = await axios('/contacts');
+        return response.data;
+      } catch (error) {
+        toast.error(`Sorry, error is occurred`, {
+          position: 'top-right',
+          theme: 'colored',
+        });
+        return thunkAPI.rejectWithValue(error.message);
+      }
+    }
+  );
+  
+  export const addContact = createAsyncThunk(
+    'contacts/addContact',
+    async ({ name, phone }, thunkAPI) => {
+      try {
+        const response = await axios.post('/contacts', { name, phone });
+        return response.data;
+      } catch (error) {
+        toast.error(`Sorry, error is occurred`, {
+          position: 'top-right',
+          theme: 'colored',
+        });
+        return thunkAPI.rejectWithValue(error.message);
+      }
+    }
+  );
+  
+  export const deleteContact = createAsyncThunk(
+    'contacts/deleteContact',
+    async (id, thunkAPI) => {
+      try {
+        const response = await axios.delete(`/contacts/${id}`);
+        return response.data;
+      } catch (error) {
+        toast.error(`Sorry, error is occurred`, {
+          position: 'top-right',
+          theme: 'colored',
+        });
+        return thunkAPI.rejectWithValue(error.message);
+      }
+    }
+  );
