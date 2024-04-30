60bca93b048fab89e8b3c07f7cca413c85bcb02d
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-04-30 23:55:20 +0200

    style app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
-import css from './App.module.css';
+/*import css from './App.module.css';*/
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
