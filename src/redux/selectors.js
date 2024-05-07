c605eb77af687f29ae36c6447587f1471579d07d
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-05-07 22:38:38 +0200

    selectors

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,6 @@
 import { createSelector } from "@reduxjs/toolkit";
 
-export const selectItems =state => state.contacts.items;
+export const selectItems = state => state.contacts.items;
+export const selectFetchContacts = state => state.contacts.isLoading.fetchContacts;
+export const selectAddContacts = state => state.contacts.isLoading.addContacts;
+export const selectDeletedContacts => state.contacts.isLoading.deletedContacts;
