a52b09dcc6e00646eae714b6b58ca5055b1c978c
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-05-09 14:18:38 +0200

    selectors

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -4,3 +4,7 @@ export const selectItems = state => state.contacts.items;
 export const selectFetchContacts = state => state.contacts.isLoading.fetchContacts;
 export const selectAddContacts = state => state.contacts.isLoading.addContacts;
 export const selectDeletedContacts = state => state.contacts.isLoading.deletedContacts;
+export const selectError = state => state.contacts.error;
+
+export const selectFilterValue = state => state.filter;
+
