04e5d09c6f8d38e712231851551bbf2f539cfc03
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-05-20 23:09:29 +0200

    filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,3 +1,4 @@
 import { setFilter } from "redux/filterSlice";
 import { selectFilterValue } from '../../redux/selectors';
 import { useDispatch, useSelector } from "react-redux";
+import css from './Filter.module.css';
