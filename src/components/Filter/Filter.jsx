085f6110b8a739b2d2fa1a20fc597f2208186535
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-05-21 11:08:38 +0200

    filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,3 +2,25 @@ import { setFilter } from "redux/filterSlice";
 import { selectFilterValue } from '../../redux/selectors';
 import { useDispatch, useSelector } from "react-redux";
 import css from './Filter.module.css';
+
+export const Filter = () => {
+    const dispatch = useDispatch();
+    const filter = useSelector(selectFilterValue);
+  
+    const handleChange = e => {
+      dispatch(setFilter(e.currentTarget.value));
+    };
+  
+    return (
+      <label className={css.label}>
+        Find contacts by name:
+        <input
+          className={css.input}
+          type="text"
+          name="filter"
+          value={filter}
+          onChange={handleChange}
+        />
+      </label>
+    );
+  };
