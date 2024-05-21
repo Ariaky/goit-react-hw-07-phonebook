9561aa3aa896a1a5f2e8fccef283ec7a1e67764e
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-05-21 11:20:29 +0200

    operation

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -8,7 +8,7 @@ export const fetchContacts = createAsyncThunk(
     'contacts/fetchAll',
     async (_, thunkAPI) => {
       try {
-        const response = await axios('/contacts');
+        const response = await axios.get('/contacts');
         return response.data;
       } catch (error) {
         toast.error(`Sorry, error is occurred`, {
