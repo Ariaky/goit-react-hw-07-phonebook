a3f5ef1cf279fb02b0c9c403a312004fd78ac20d
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-05-22 23:03:34 +0200

    operations

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from 'react-toastify';
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = 'https://6630049dc92f351c03d8d7aa.mockapi.io';
+axios.defaults.baseURL = 'https://6630049dc92f351c03d8d7aa.mockapi.io/';
 
 export const fetchContacts = createAsyncThunk(
     'contacts/fetchAll',
