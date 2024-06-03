f63e43c9a7fdf4c318276fcd43381e973575ba27
Maryna Dubois Khliebnikova <90453674+Ariaky@users.noreply.github.com>
2024-06-03 23:19:42 +0200

    commit

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,7 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-/*import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import initialContacts from '../components/data/contacts.json'*/
 import { fetchContacts, addContact, deleteContact } from "./operations";
 
 const contactsInitialState = {
@@ -13,37 +10,6 @@ const contactsInitialState = {
     },
     error: null,
 };
-
-/*const contactSlice = createSlice({
-  name: 'contacts',
-  initialState: { 
-    list: contactsInitialState,
-    filter: ''
-  },
-    reducers: {
-    addContact(state, action) {
-      state.list.push(action.payload);
-    },
-    deleteContact(state, action) {
-      state.list = state.list.filter(contact => contact.id !== action.payload);
-    },
-    updateFilter(state, action) {
-      state.filter = action.payload;
-    }
-  }
-});*/
-
-/*const persistConfig = {
-  key: 'contacts',
-  storage,
-};
-
-export const { addContact, deleteContact, updateFilter } = contactSlice.actions;
-
-export const contactReducer = persistReducer(
-  persistConfig,
-  contactSlice.reducer
-);*/
 
 const contactsSlice = createSlice({
     name: 'contacts',
